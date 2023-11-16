d87ffa3c3932002b1bed9f292f22d7c56287caf3
joacow <joaquinwisgikl@gmail.com>
2023-11-16 08:50:47 -0300

    Se realizaron rutas y tipos para la api

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express from "express"
+
+import diaryRouter from "./routes/diaries"
+
+const app = express()
+app.use(express.json())//Middleware que transforma la req.body a un json
+
+const PORT = 3000
+
+app.get("/ping", (_,res)=>{
+    console.log("Someone pinged here!!" + new Date().toLocaleDateString())
+    res.send("pong")
+})
+
+app.use("/api/diaries", diaryRouter)
+
+app.listen(PORT, ()=>{
+    console.log(`Server running on port ${PORT}`)
+})
